8c4e48631a7f20339a3a2c0aafda8957035f9f1d
pv <pat@cs.umass.edu>
2016-03-04 16:22:42 -0500

    add flags and bidirectional option

--- src/tf/ds-classifer.py
+++ src/tf/ds-classifer.py
@@ -1,33 +1,39 @@
 import tensorflow as tf
+import time
 from tensorflow.models.rnn import rnn
 import numpy as np
 from collections import defaultdict
 import sys
 from random import shuffle
+
 #
 #         Params
 #
-np.random.seed(10)
+flags = tf.flags
+flags.DEFINE_string("in_file", 'data/data-min25.ints', "input file")
+flags.DEFINE_integer("gpuid", 0, "gpu id to use")
+flags.DEFINE_integer("seed", 0, "random seed")
+flags.DEFINE_integer("pad_token", 2, "int mapping for pad token")
+flags.DEFINE_integer("batch_size", 1000, "max mini batch size")
+flags.DEFINE_integer("word_dim", 50, "dimension for word embeddings")
+flags.DEFINE_integer("hidden_dim", 100, "dimension for hidden state")
+flags.DEFINE_integer("max_grad_norm", 100, "maximum gradient norm")
+flags.DEFINE_integer("num_layers", 1, "number of layers for network")
+flags.DEFINE_integer("max_epoch", 25, "number of epochs to run for")
+flags.DEFINE_boolean("bi", False, "Use bi-directional lstm")
+flags.DEFINE_float("lr", .1, "initial learning rate")
+flags.DEFINE_float("lr_decay", .2, "learning rate decay")
+flags.DEFINE_float("dropout", .2, "dropout probability")
+FLAGS = flags.FLAGS
 
-# in_file = 'data/data-min25.ints'
-in_file = 'data/data-min25.ints'
-pad_token = 2
-batch_size = 250
-word_dim = 50
-hidden_dim = 100
-learning_rate = .1
-lr_decay = .2
-num_layers = 1
-dropout_prob = .2
-max_grad_norm = 100
+np.random.seed(FLAGS.seed)
+tf.set_random_seed(FLAGS.seed)
+cur_size = FLAGS.batch_size
 is_training = True
-max_epoch = 1
-max_max_epoch = 25
 
 #
 #          Data
 #
-
 
 ep_pattern_map = defaultdict(list)
 pattern_token_map = defaultdict(list)
@@ -36,97 +42,107 @@ vocab_size = 0
 label_size = 0
 data_x = []
 data_y = []
-with open(in_file) as f:
+with open(FLAGS.in_file) as f:
     for i, line in enumerate(f):
         e1, e2, ep, pattern, tokens, label = line.strip().split('\t')
         ep_pattern_map[ep].append(pattern)
-        label = int(label)-1
-        label_size = max(label_size, label+1)
+        label = int(label) - 1
+        label_size = max(label_size, label + 1)
         token_list = map(int, tokens.split(' '))
         if len(token_list) <= seq_len:
             if len(token_list) < seq_len:
-                token_list += [pad_token] * (seq_len - len(token_list))
+                token_list += [FLAGS.pad_token] * (seq_len - len(token_list))
             pattern_token_map[pattern].append(token_list)
-            vocab_size = max(vocab_size, max(token_list)+1)
+            vocab_size = max(vocab_size, max(token_list) + 1)
             data_x.append(token_list)
             data_y.append(label)
 
+print(str(len(data_x)) + ' examples\t'
+      + str(len(ep_pattern_map)) + ' entity pairs\t'
+      + str(len(pattern_token_map)) + ' patterns\t'
+      + str(label_size) + ' labels\t'
+      + str(vocab_size) + ' unique tokens')
 
-    print(str(i) + ' examples\t'
-          + str(len(ep_pattern_map)) + ' entity pairs\t'
-          + str(len(pattern_token_map)) + ' patterns\t'
-          + str(label_size) + ' labels\t'
-          + str(vocab_size) + ' unique tokens')
 
-# shuffle the training data
-zipped_data = zip(data_x, data_y)
-shuffle(zipped_data)
-data_x, data_y = zip(*zipped_data)
+def shuffle_data(x_dat, y_dat):
+    zipped_data = zip(x_dat, y_dat)
+    shuffle(zipped_data)
+    x_dat, y_dat = zip(*zipped_data)
+    return x_dat, y_dat
+
+
+data_x, data_y = shuffle_data(data_x, data_y)
 
 # convert data to numpy arrays - labels must be dense one-hot vectors
 dense_y = []
 for i, j in enumerate(data_y):
-    dense_y.append([0]*label_size)
+    dense_y.append([0] * label_size)
     dense_y[i][j] = 1
 data_y = np.array(dense_y)
 data_x = np.array(data_x)
-train_x, dev_x = data_x[:-1000], data_x[-1000+1:]
-train_y, dev_y = data_y[:-1000], data_y[-1000+1:]
-
-
-
+train_x, dev_x = data_x[:-1000], data_x[-1000:]
+train_y, dev_y = data_y[:-1000], data_y[-1000:]
 
 #
 #       Model stuff
 #
+with tf.device('/gpu:'+str(FLAGS.gpuid)):
 
-input_x = tf.placeholder(tf.int32, [None, seq_len], name="input_x")
-input_y = tf.placeholder(tf.float32, [None, label_size], name="input_y")
-# sx, sy = tf.shape(input_x), tf.shape(input_y)
-# input_x = tf.Print(input_x, [sx, sy])
+    input_x = tf.placeholder(tf.int32, [None, seq_len], name="input_x")
+    input_y = tf.placeholder(tf.float32, [None, label_size], name="input_y")
+    # sx, sy = tf.shape(input_x), tf.shape(input_y)
+    # input_x = tf.Print(input_x, [sx, sy])
 
-lookup_table = tf.Variable(tf.random_uniform([vocab_size, word_dim], -1.0, 1.0))
-inputs = tf.nn.embedding_lookup(lookup_table, input_x)
-inputs = tf.nn.dropout(inputs, 1 - dropout_prob)
-inputs = [tf.squeeze(input_, [1]) for input_ in tf.split(1, seq_len, inputs)]
+    with tf.device('/cpu:0'):
+        lookup_table = tf.Variable(tf.random_uniform([vocab_size, FLAGS.word_dim], -1.0, 1.0))
+        inputs = tf.nn.embedding_lookup(lookup_table, input_x)
+    inputs = tf.nn.dropout(inputs, 1 - FLAGS.dropout)
+    inputs = [tf.squeeze(input_, [1]) for input_ in tf.split(1, seq_len, inputs)]
 
-lstm_cell = tf.nn.rnn_cell.LSTMCell(num_units=hidden_dim, input_size=word_dim)
-if is_training and 1 - dropout_prob < 1:
-    lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=1 - dropout_prob)
-cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * num_layers)
+    lstm_cell = tf.nn.rnn_cell.LSTMCell(num_units=FLAGS.hidden_dim, input_size=FLAGS.word_dim)
+    if is_training and 1 - FLAGS.dropout < 1:
+        lstm_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=1 - FLAGS.dropout)
+    cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * FLAGS.num_layers)
+    if FLAGS.bi:
+        back_cell = tf.nn.rnn_cell.LSTMCell(num_units=FLAGS.hidden_dim, input_size=FLAGS.word_dim)
+        if is_training and 1 - FLAGS.dropout < 1:
+            back_cell = tf.nn.rnn_cell.DropoutWrapper(lstm_cell, output_keep_prob=1 - FLAGS.dropout)
+        back_cell = tf.nn.rnn_cell.MultiRNNCell([back_cell] * FLAGS.num_layers)
+        outputs = rnn.bidirectional_rnn(cell, back_cell, inputs, dtype=tf.float32)
+        state = outputs[-1] + outputs[len(outputs)/2]
+    else:
+        outputs, state = rnn.rnn(cell, inputs, dtype=tf.float32)
 
-outputs, state = rnn.rnn(cell, inputs, initial_state=cell.zero_state(batch_size, tf.float32))
-# lstm returns [hiddenstate+cell] -- extact just the hidden state
-state = tf.slice(state, [0, 0], [batch_size, hidden_dim])
-softmax_w = tf.get_variable("softmax_w", [hidden_dim, label_size])
-softmax_b = tf.get_variable("softmax_b", [label_size])
+    # lstm returns [hiddenstate+cell] -- extact just the hidden state
+    # state = tf.slice(state, [0, 0], [cur_size, FLAGS.hidden_dim])
+    state = tf.slice(state, [0, 0], [FLAGS.batch_size, FLAGS.hidden_dim])
+    softmax_w = tf.get_variable("softmax_w", [FLAGS.hidden_dim, label_size])
+    softmax_b = tf.get_variable("softmax_b", [label_size])
 
-logits = tf.nn.xw_plus_b(state, softmax_w, softmax_b, name="logits")
-# training loss
-loss = tf.nn.softmax_cross_entropy_with_logits(logits, input_y)
-_cost = tf.reduce_sum(loss) / batch_size
+    logits = tf.nn.xw_plus_b(state, softmax_w, softmax_b, name="logits")
+    # training loss
+    loss = tf.nn.softmax_cross_entropy_with_logits(logits, input_y)
+    # _cost = tf.reduce_sum(loss) / cur_size
+    _cost = tf.reduce_sum(loss) / FLAGS.batch_size
 
-tvars = tf.trainable_variables()
-grads, _ = tf.clip_by_global_norm(tf.gradients(_cost, tvars), max_grad_norm)
-# optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+    tvars = tf.trainable_variables()
+    grads, _ = tf.clip_by_global_norm(tf.gradients(_cost, tvars), FLAGS.max_grad_norm)
+    optimizer = tf.train.AdamOptimizer(FLAGS.lr)
+    _train_op = optimizer.apply_gradients(zip(grads, tvars))
 
-optimizer = tf.train.AdamOptimizer(learning_rate)
-# grads_and_vars = optimizer.compute_gradients(loss)
-# _train_op = optimizer.apply_gradients(grads_and_vars)
-_train_op = optimizer.apply_gradients(zip(grads, tvars))
+    # eval
+    correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(input_y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# eval
-correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(input_y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 #
 #           Train
 #
-class MinibatchIterator:
-    def __init__(self, data_array, batch_size):
+class BatchIter:
+    def __init__(self, data_array, size):
         self.data_array = data_array
         self.num_rows = data_array.shape[0]
-        self.batch_size = batch_size
+        self.batch_size = size
         self.start_idx = 0
 
     def __iter__(self):
@@ -143,35 +159,33 @@ class MinibatchIterator:
             return to_return
 
 
-with tf.Graph().as_default() and tf.Session() as session:
+with tf.Graph().as_default() and tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
     tf.initialize_all_variables().run()
 
-    for i in range(max_max_epoch):
-        print 'Evaluating'
+    for i in range(FLAGS.max_epoch):
         is_training = False
-        x_batch, y_batch = MinibatchIterator(dev_x, batch_size), MinibatchIterator(dev_y, batch_size)
         accuracies = []
-        for step, (x, y) in enumerate(zip(x_batch, y_batch)):
-            if len(x) == batch_size and int(np.sum(y)) > 0:
+        for step, (x, y) in enumerate(zip(BatchIter(dev_x, FLAGS.batch_size), BatchIter(dev_y, FLAGS.batch_size))):
+            cur_size = len(x)
+            if cur_size == FLAGS.batch_size:
                 accuracies += session.run([accuracy], feed_dict={input_x: x, input_y: y})
-        print 'Accuracy : ' + str(reduce(lambda x, y: x + y, accuracies) / len(accuracies))
+        print '\nAccuracy : ' + str(reduce(lambda a, b: a+b, accuracies) / len(accuracies))
 
         print 'Training'
-        lr_decay = lr_decay ** max(i - max_epoch, 0.0)
-        x_batch, y_batch = MinibatchIterator(data_x, batch_size), MinibatchIterator(data_y, batch_size)
+        FLAGS.lr_decay = FLAGS.lr_decay ** max(i - FLAGS.max_epoch, 0.0)
+        # train_x, train_y = shuffle_data(train_x, train_y)
         costs = []
+        start_time = time.time()
         is_training = True
-        for step, (x, y) in enumerate(zip(x_batch, y_batch)):
-            if len(x) != batch_size:
-                print '\nbatchsize mismatch'
-            elif int(np.sum(y)) == 0:
+        for step, (x, y) in enumerate(zip(BatchIter(train_x, FLAGS.batch_size), BatchIter(train_y, FLAGS.batch_size))):
+            if int(np.sum(y)) == 0:
                 print 'no label!', np.sum(x), np.sum(y)
             else:
-                cost, _ = session.run([_cost, _train_op], feed_dict={input_x: x, input_y: y})
-                costs.append(cost)
-                sys.stdout.write('\r{:4.3f} last cost, {:4.3f} avg cost, {:1.0f} step'.format(cost, reduce(lambda x, y: x + y, costs) / len(costs), step))
-                sys.stdout.flush()
-
-
-
-
+                cur_size = len(x)
+                if cur_size == FLAGS.batch_size:
+                    cost, _ = session.run([_cost, _train_op], feed_dict={input_x: x, input_y: y})
+                    costs.append(cost)
+                    exp_per_sec = FLAGS.batch_size * ((time.time() - start_time) * 1000 / (step + 1))
+                    sys.stdout.write('\r{:4.3f} last err, {:4.3f} avg err, {:2.2f} % done, examples/sec {:0.3f}'
+                         .format(cost, reduce(lambda a, b: a+b, costs) / len(costs), (100*step*FLAGS.batch_size/float(len(train_x))), exp_per_sec))
+                    sys.stdout.flush()
