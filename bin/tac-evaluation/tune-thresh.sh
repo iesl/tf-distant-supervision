a7ed8101357576c77b3fa068930de381ea46235c
pv <pat@cs.umass.edu>
2016-03-06 17:45:22 -0500

    tune thresh for already scored file

--- bin/tac-evaluation/tune-thresh.sh
+++ bin/tac-evaluation/tune-thresh.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 
 YEAR=$1
-MODEL=$2
-VOCAB=$3
-GPU=$4
-MAX_SEQ=$5
-OUT=$6
-EVAL_ARGS=${@:7}
+SCORED_CANDIDATES=$2
+OUT=$3
+EVAL_ARGS=${@:4}
 
 THRESHOLDS="0.0 0.1 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.7 0.75 0.8 0.9 1.0"
 
 TAC_EVAL_ROOT=${TH_RELEX_ROOT}/bin/tac-evaluation
 source ${TAC_EVAL_ROOT}/configs/${YEAR}
-
-# score candidate file
-SCORED_CANDIDATES=`mktemp`
-CAND_SCORE_CMD="th ${TH_RELEX_ROOT}/src/eval/ScoreCandidateFile.lua -candidates $CANDIDATES -vocabFile $VOCAB -model $MODEL -gpuid $GPU -threshold 0 -outFile $SCORED_CANDIDATES -maxSeq $MAX_SEQ $EVAL_ARGS"
-echo "Scoring candidate file: ${CAND_SCORE_CMD}"
-${CAND_SCORE_CMD}
-
 
 TMP_DIR=`mktemp -d`
 echo "Working in temp directory $TMP_DIR"
