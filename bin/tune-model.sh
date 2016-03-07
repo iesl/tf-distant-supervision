134c134f4f580c4619bc98a678b8334c954e460e
pv <pat@cs.umass.edu>
2016-03-06 20:14:40 -0500

    hyper param tuning tac eval hopefully working in subprocess

---
+++ bin/tune-model.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+#!/bin/sh
+
+export MAX_EPOCHS=6
+export EVAL_FREQ=3
+
+OUT_LOG=results/hyperparams/
+mkdir -p $OUT_LOG
+echo "Writing to "$OUT_LOG
+
+# run on all available gpus
+#gpus=`nvidia-smi -L | wc -l`
+gpuids=( 0 1 2 3 )
+num_gpus=${#gpuids[@]}
+
+# grid search over these
+lrs="0.001 0.01 0.1"
+lr_decays="0.1 0.01"
+dropouts="0.1 0.25 0.5"
+clipgrads="100" # 0.1 0.25"
+word_dims="10 25 50"
+hidden_dims="10 25 50"
+batchsizes="1024 256"
+
+RUN_CMD="python src/tf/ds-classifer.py "
+
+# array to hold all the commands we'll distribute
+declare -a commands
+
+# first make all the commands we want
+for word_dim in $word_dims
+do
+   for lr in $lrs
+   do
+       for decay in $lr_decays
+       do
+           for hidden_dim in $hidden_dims
+           do
+               for batchsize in $batchsizes;
+               do
+                   for clipgrad in $clipgrads;
+                   do
+                       for dropout in $dropouts;
+                       do
+                           RUN_NAME=$word_dim-$hidden_dim-$lr-$decay-$dropout-$clipgrad-$batchsize
+                           CMD="$RUN_CMD \
+                                --word_dim $word_dim \
+                                --hidden_dim $hidden_dim \
+                                --lr $lr \
+                                --lr_decay $decay \
+                                --batch_size $batchsize \
+                                --dropout $dropout \
+                                --max_grad_norm $clipgrad \
+                                --result_dir $RUN_NAME\
+                                --gpuid 0 \
+                                --max_epoch $MAX_EPOCHS \
+                                --tac_eval_freq $EVAL_FREQ \
+                                &> $OUT_LOG/train-$RUN_NAME.log"
+                           commands+=("$CMD")
+                           echo "Adding job $RUN_NAME"
+                        done
+                    done
+               done
+           done
+       done
+   done
+done
+
+# now distribute them to the gpus
+#
+# currently this is only correct if the number of jobs is a 
+# multiple of the number of gpus (true as long as you have hyperparams
+# ranging over 2, 3 and 4 values)!
+num_jobs=${#commands[@]}
+jobs_per_gpu=$((num_jobs / num_gpus))
+echo "Distributing $num_jobs jobs to $num_gpus gpus ($jobs_per_gpu jobs/gpu)"
+
+j=0
+for gpuid in ${gpuids[@]}; do
+    for (( i=0; i<$jobs_per_gpu; i++ )); do
+        jobid=$((j * jobs_per_gpu + i))
+        comm="${commands[$jobid]/XX/$gpuid}"
+        echo "Starting job $jobid on gpu $gpuid"
+        if [ "$gpuid" -ge 0 ]; then
+            comm="CUDA_VISIBLE_DEVICES=$gpuid $comm"
+        fi
+        eval ${comm}
+    done &
+    j=$((j + 1))
+done
--- src/tf/ds-classifer.py
+++ src/tf/ds-classifer.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 import sys
 from random import shuffle
 import subprocess
-
+import os
 #
 #         Params
 #
@@ -15,21 +15,21 @@ flags.DEFINE_string("in_file", 'data/merge_2013.tab_min25.ints', "input file")
 # tac related
 flags.DEFINE_string("int_file", 'data/candidates/candidates_2012.wc.ints', "int mapped input file")
 flags.DEFINE_string("candidate_file", 'data/candidates/candidates_2012', "original candidate file")
-flags.DEFINE_string("scored_file", 'tmp_out', "output for scored candidate file")
+flags.DEFINE_string("result_dir", 'results/tmp', "output for scored candidate file")
 
 flags.DEFINE_integer("gpuid", 0, "gpu id to use")
 flags.DEFINE_integer("seed", 0, "random seed")
 flags.DEFINE_integer("dev_samples", 1000, "number of training instances to hold out to calculate dev accuracy")
 flags.DEFINE_integer("pad_token", 2, "int mapping for pad token")
 flags.DEFINE_integer("batch_size", 1000, "max mini batch size")
-flags.DEFINE_integer("word_dim", 50, "dimension for word embeddings")
-flags.DEFINE_integer("hidden_dim", 100, "dimension for hidden state")
+flags.DEFINE_integer("word_dim", 25, "dimension for word embeddings")
+flags.DEFINE_integer("hidden_dim", 20, "dimension for hidden state")
 flags.DEFINE_integer("max_grad_norm", 100, "maximum gradient norm")
 flags.DEFINE_integer("num_layers", 1, "number of layers for network")
 flags.DEFINE_integer("max_epoch", 25, "number of epochs to run for")
 flags.DEFINE_integer("tac_eval_freq", 5, "run tac evaluation every kth iteration")
 flags.DEFINE_integer("seq_len", 50, "max length of token sequences")
-flags.DEFINE_boolean("bi", False, "Use bi-directional lstm")
+flags.DEFINE_boolean("bi", True, "Use bi-directional lstm")
 flags.DEFINE_float("lr", .1, "initial learning rate")
 flags.DEFINE_float("lr_decay", .2, "learning rate decay")
 flags.DEFINE_float("dropout", .2, "dropout probability")
@@ -38,51 +38,45 @@ FLAGS = flags.FLAGS
 np.random.seed(FLAGS.seed)
 tf.set_random_seed(FLAGS.seed)
 is_training = True
+gpu_mem_fraction = .33
 
 
 #
 #          Data
 #
 def read_int_file(int_file):
-    ep_pat_map, pat_token_map = defaultdict(list), defaultdict(list)
+    ep_pat_map = defaultdict(list)
     l_size, v_size, line_count, too_long = 0, 0, 0, 0
     x, y = [], []
     with open(int_file) as f:
         for line in f:
             line_count += 1
             e1, e2, ep, pattern, tokens, label = line.strip().split('\t')
-            ep_pat_map[ep].append(pattern)
             label = int(label) - 1
             l_size = max(l_size, label + 1)
             token_list = map(int, tokens.strip().split(' '))
             if len(token_list) <= FLAGS.seq_len:
                 if len(token_list) < FLAGS.seq_len:
                     token_list += [FLAGS.pad_token] * (FLAGS.seq_len - len(token_list))
-                pat_token_map[pattern].append(token_list)
+                ep_pat_map[ep].append(token_list)
                 v_size = max(v_size, max(token_list) + 1)
                 x.append(token_list)
                 y.append(label)
             else:
                 too_long += 1
     print 'Read in ' + str(line_count) + ' lines. ' + str(too_long) + ' lines were greater than max seq length.'
-    return x, y, ep_pat_map, pat_token_map, l_size, v_size
+    return x, y, ep_pat_map, l_size, v_size
 
-data_x, data_y, ep_pattern_map, pattern_token_map, label_size, vocab_size = read_int_file(FLAGS.in_file)
+data_x, data_y, ep_pattern_map, label_size, vocab_size = read_int_file(FLAGS.in_file)
 
 print(str(len(data_x)) + ' examples\t'
       + str(len(ep_pattern_map)) + ' entity pairs\t'
-      + str(len(pattern_token_map)) + ' patterns\t'
       + str(label_size) + ' labels\t'
       + str(vocab_size) + ' unique tokens')
 
-
-def shuffle_data(x_dat, y_dat):
-    zipped_data = zip(x_dat, y_dat)
-    shuffle(zipped_data)
-    return zip(*zipped_data)
-
-
-data_x, data_y = shuffle_data(data_x, data_y)
+zipped_data = zip(data_x, data_y)
+shuffle(zipped_data)
+data_x, data_y = zip(*zipped_data)
 
 # convert data to numpy arrays - labels must be dense one-hot vectors
 dense_y = []
@@ -98,7 +92,6 @@ train_y, dev_y = data_y[:-FLAGS.dev_samples], data_y[-FLAGS.dev_samples:]
 #       Model stuff
 #
 with tf.device('/gpu:'+str(FLAGS.gpuid)):
-
     batch_size = tf.placeholder(tf.float32)
     input_x = tf.placeholder(tf.int32, [None, FLAGS.seq_len], name="input_x")
     input_y = tf.placeholder(tf.float32, [None, label_size], name="input_y")
@@ -134,7 +127,7 @@ with tf.device('/gpu:'+str(FLAGS.gpuid)):
     _cost = tf.reduce_sum(loss) / batch_size
 
     tvars = tf.trainable_variables()
-    grads, _ = tf.clip_by_global_norm(tf.gradients(_cost, tvars), FLAGS.max_grad_norm)
+    grads, _ = tf.clip_by_global_norm(tf.gradients(_cost, tvars, aggregation_method=2), FLAGS.max_grad_norm)
     optimizer = tf.train.AdamOptimizer(FLAGS.lr)
     _train_op = optimizer.apply_gradients(zip(grads, tvars))
 
@@ -168,8 +161,8 @@ class BatchIter:
 
 
 # score tac candidate file
-def score_tac():
-    tac_x, tac_y, tac_ep_pattern_map, tac_pattern_token_map, _, _ = read_int_file(FLAGS.int_file)
+def score_tac(_epoch):
+    tac_x, tac_y, tac_ep_pattern_map, _, _ = read_int_file(FLAGS.int_file)
     dense_tac_y = []
     for i, j in enumerate(tac_y):
         dense_tac_y.append([0] * label_size)
@@ -196,11 +189,17 @@ def score_tac():
     delta = max_score - min_score
     scores = [(s-min_score)/delta for s in scores]
 
-    with open(FLAGS.scored_file, 'w') as f:
+    scored_candidate = FLAGS.result_dir+'/scored_'+str(epoch)
+    if not os.path.exists(FLAGS.result_dir):
+        os.makedirs(FLAGS.result_dir)
+    with open(scored_candidate, 'w') as f:
         for score, out_line in zip(scores, out_lines):
             f.write(out_line + '\t' + str(score) + '\n')
+        subprocess.Popen('bin/tac-evaluation/tune-thresh.sh 2012 ' + scored_candidate + ' ' + FLAGS.result_dir + '/tuned_' + str(_epoch) + ' &', shell=True)
 
-with tf.Graph().as_default() and tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as session:
+with tf.Graph().as_default() and tf.Session(
+        config=tf.ConfigProto(allow_soft_placement=True,
+                              gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=gpu_mem_fraction))) as session:
     tf.initialize_all_variables().run()
 
     for epoch in range(1, FLAGS.max_epoch):
@@ -212,12 +211,13 @@ with tf.Graph().as_default() and tf.Session(config=tf.ConfigProto(allow_soft_pla
 
         if epoch % FLAGS.tac_eval_freq == 0:
             print('\nScoring tac candidate file :' + FLAGS.candidate_file)
-            score_tac()
+            score_tac(epoch)
 
         print 'Training - epoch : ' + str(epoch)
         FLAGS.lr_decay = FLAGS.lr_decay ** max(epoch - FLAGS.max_epoch, 0.0)
         # train_x, train_y = shuffle_data(train_x, train_y)
         costs = []
+        total_cost = 0
         start_time = time.time()
         is_training = True
         # shuffle training data
@@ -228,9 +228,11 @@ with tf.Graph().as_default() and tf.Session(config=tf.ConfigProto(allow_soft_pla
             if int(np.sum(y)) == 0:
                 print 'no label!', np.sum(x), np.sum(y)
             else:
-                cost, _ = session.run([_cost, _train_op], feed_dict={input_x: x, input_y: y, batch_size:len(x)})
+                # print len(x)
+                cost, _ = session.run([_cost, _train_op], feed_dict={input_x: x, input_y: y, batch_size: len(x)})
                 costs.append(cost)
+                total_cost += cost
                 exp_per_sec = FLAGS.batch_size * ((time.time() - start_time) * 1000 / (step + 1))
                 sys.stdout.write('\r{:4.3f} last err, {:4.3f} avg err, {:2.2f} % done, examples/sec {:0.3f}'
-                     .format(cost, reduce(lambda a, b: a+b, costs) / len(costs), (100*step*FLAGS.batch_size/float(len(train_x))), exp_per_sec))
+                     .format(cost, total_cost / len(costs), (100*step*FLAGS.batch_size/float(len(train_x))), exp_per_sec))
                 sys.stdout.flush()
